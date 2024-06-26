932d1bb746ffac001975b110ae3bda124af0a1b9
Johnnynaorem <johnnynaorem7@gmail.com>
2024-06-26 09:16:43 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,11 +1,24 @@
 import mongoose from 'mongoose';
+
 const connectDB = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URL)
-    console.log(`Connected to mongoDB ${con.connection.host}`.bgMagenta.black)
+    const mongoURI = process.env.MONGO_URL;
+    
+    if (!mongoURI) {
+      throw new Error('MongoDB connection string is not provided in environment variables.');
+    }
+
+    const conn = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+
+    console.log(`Connected to MongoDB: ${conn.connection.host}`.bgMagenta.black);
   } catch (error) {
-    console.log(`Error in MongoDB ${error}`)
+    console.error(`Error connecting to MongoDB: ${error.message}`);
   }
-}
+};
 
 export default connectDB;
